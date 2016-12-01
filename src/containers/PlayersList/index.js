b2e2bb789d086a5497b565eb8c68fa1fbe2b83ed
Denis Novikov (dehimer) <den-vse@yandex.ru>
2016-12-01 13:41:24 +0300

    view of playerslist

--- src/containers/PlayersList/index.js
+++ src/containers/PlayersList/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Table, Button } from 'react-bootstrap'
+import { Grid, Row, Col, Table, Button } from 'react-bootstrap'
 
 class PlayersList extends Component {
   constructor(props) {
@@ -16,35 +16,45 @@ class PlayersList extends Component {
     const players = this.props.players;
     return (
       <div>
-        <Button
-          onClick={ ::this.removeAllPlayers }>
-          Удалить всех
-        </Button>
-        <Table responsive>
-          <thead>
-            <tr>
-              <th>Цвет</th>
-              <th>Номер</th>
-              <th>Имя</th>
-              <th>Email</th>
-              <th>Баллы</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              players.map((player, index) => {
-                const playerNum = parseInt(player.color, 16);
-                return (<tr key={ index }>
-                  <th><div style={{backgroundColor:'#'+this.props.colorsById[player.colorId].code, width:'20px', height:'20px'}}></div></th>
-                  <th>{ playerNum }</th>
-                  <th>{ player.name }</th>
-                  <th>{ player.email }</th>
-                  <th>{ player.scores }</th>
-                </tr>)
-              })
-            }
-          </tbody>
-        </Table>
+        <Grid>
+          <Row className='show-grid'>
+            <Col>
+              <Button
+                onClick={ ::this.removeAllPlayers }>
+                Удалить всех
+              </Button>
+            </Col>
+          </Row>
+          <Row className='show-grid'>
+            <Col>
+              <Table responsive>
+                <thead>
+                  <tr>
+                    <th>Цвет</th>
+                    <th>Номер</th>
+                    <th>Имя</th>
+                    <th>Email</th>
+                    <th>Баллы</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    players.map((player, index) => {
+                      const playerNum = parseInt(player.color, 16);
+                      return (<tr key={ index }>
+                        <th><div style={{backgroundColor:'#'+this.props.colorsById[player.colorId].code, width:'20px', height:'20px'}}></div></th>
+                        <th>{ playerNum }</th>
+                        <th>{ player.name }</th>
+                        <th>{ player.email }</th>
+                        <th>{ player.scores }</th>
+                      </tr>)
+                    })
+                  }
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
+        </Grid>
       </div>
     )
   }
