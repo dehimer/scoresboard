af08f8ad42dd81d801d7de68eabd5b5f8b7cfa39
Denis Novikov <den-vse@yandex.ru>
2018-09-28 01:53:50 +0300

    feature: fill new player form with blank fields

--- src/containers/Registration/components/NewPlayer/index.js
+++ src/containers/Registration/components/NewPlayer/index.js
@@ -4,5 +4,19 @@ import './index.scss'
 
 export default (props) => {
   console.log(props);
-  return (<div className='new-player'>New Player</div>)
+  return (
+    <form className='new-player'>
+      <h3 className='new-player__header'>Не регистрировался на сайте</h3>
+      <input className='new-player__input' type='text' placeholder='Никнейм'/>
+      <input className='new-player__input' type='text' placeholder='ФИО'/>
+      <input className='new-player__input' type='text' placeholder='Дата рождения'/>
+      <input className='new-player__input' type='text' placeholder='Город проживания'/>
+      <input className='new-player__input' type='email' placeholder='E-mail'/>
+      <input className='new-player__input' type='phone' placeholder='Телефон'/>
+      <input className='new-player__input' type='text' placeholder='Ноутбук (марка и модель)'/>
+      <input className='new-player__input' type='text' placeholder='Ссылка на страницу в соц. сетях'/>
+      <input className='new-player__input' type='checkbox'>Принёс ноутбук</input>
+      <input className='new-player__input' type='button' value='Зарегистрировать и показать ID'/>
+    </form>
+  )
 };
