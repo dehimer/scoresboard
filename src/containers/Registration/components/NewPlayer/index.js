fa7da1f1ff97aa0dd247711544e86b622350f9e9
Denis Novikov <den-vse@yandex.ru>
2018-09-29 12:14:51 +0300

    feature: wrap childrens in cards

--- src/containers/Registration/components/NewPlayer/index.js
+++ src/containers/Registration/components/NewPlayer/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, TextField, FormControlLabel, Switch } from '@material-ui/core'
+import {Button, TextField, FormControlLabel, Switch, CardContent, Card} from '@material-ui/core'
 
 import './index.scss'
 
@@ -34,71 +34,75 @@ export default class NewPlayer extends Component {
     const disabled = Object.keys(this.state).some(fieldName => (this.requiredFields[fieldName] && !this.state[fieldName]));
 
     return (
-      <form className='new-player'>
-        <h3 className='new-player__header'>Не регистрировался на сайте</h3>
-        <TextField
-          className='new-player__input' type='text' label='Никнейм'
-          variant='outlined' margin='dense'
-          value={this.state.nickname}
-          onChange={this.handleChange('nickname')}
-        />
-        <TextField
-          className='new-player__input' type='text' label='ФИО'
-          variant='outlined' margin='dense'
-          value={this.state.fullname}
-          onChange={this.handleChange('fullname')}
-        />
-        <TextField
-          className='new-player__input' type='date' label='Дата рождения'
-          variant='outlined' margin='dense'
-          InputLabelProps={{
-            shrink: true
-          }}
-          value={this.state.birthday}
-          onChange={this.handleChange('birthday')}
-        />
-        <TextField
-          className='new-player__input' type='text' label='Город проживания'
-          variant='outlined' margin='dense'
-          value={this.state.city}
-          onChange={this.handleChange('city')}
-        />
-        <TextField
-          className='new-player__input' type='email' label='E-mail'
-          variant='outlined' margin='dense'
-          value={this.state.email}
-          onChange={this.handleChange('email')}
-        />
-        <TextField
-          className='new-player__input' type='phone' label='Телефон'
-          variant='outlined' margin='dense'
-          value={this.state.phone}
-          onChange={this.handleChange('phone')}
-        />
-        <TextField
-          className='new-player__input' type='text' label='Ноутбук (марка и модель)'
-          variant='outlined' margin='dense'
-          value={this.state.notebook}
-          onChange={this.handleChange('notebook')}
-        />
-        <TextField
-          className='new-player__input' type='text' label='Ссылка на страницу в соц. сетях'
-          variant='outlined' margin='dense'
-          value={this.state.link}
-          onChange={this.handleChange('link')}
-        />
-        <FormControlLabel
-          className='new-player__input'
-          control={
-            <Switch checked={this.state.broughtNotebook} value='broughtNotebook' onChange={::this.handleChange('broughtNotebook')}/>
-          }
-          label='Принёс ноутбук'
-        />
+      <Card>
+        <CardContent>
+          <form className='new-player'>
+            <h3 className='new-player__header'>Не регистрировался на сайте</h3>
+            <TextField
+              className='new-player__input' type='text' label='Никнейм'
+              variant='outlined' margin='dense'
+              value={this.state.nickname}
+              onChange={this.handleChange('nickname')}
+            />
+            <TextField
+              className='new-player__input' type='text' label='ФИО'
+              variant='outlined' margin='dense'
+              value={this.state.fullname}
+              onChange={this.handleChange('fullname')}
+            />
+            <TextField
+              className='new-player__input' type='date' label='Дата рождения'
+              variant='outlined' margin='dense'
+              InputLabelProps={{
+                shrink: true
+              }}
+              value={this.state.birthday}
+              onChange={this.handleChange('birthday')}
+            />
+            <TextField
+              className='new-player__input' type='text' label='Город проживания'
+              variant='outlined' margin='dense'
+              value={this.state.city}
+              onChange={this.handleChange('city')}
+            />
+            <TextField
+              className='new-player__input' type='email' label='E-mail'
+              variant='outlined' margin='dense'
+              value={this.state.email}
+              onChange={this.handleChange('email')}
+            />
+            <TextField
+              className='new-player__input' type='phone' label='Телефон'
+              variant='outlined' margin='dense'
+              value={this.state.phone}
+              onChange={this.handleChange('phone')}
+            />
+            <TextField
+              className='new-player__input' type='text' label='Ноутбук (марка и модель)'
+              variant='outlined' margin='dense'
+              value={this.state.notebook}
+              onChange={this.handleChange('notebook')}
+            />
+            <TextField
+              className='new-player__input' type='text' label='Ссылка на страницу в соц. сетях'
+              variant='outlined' margin='dense'
+              value={this.state.link}
+              onChange={this.handleChange('link')}
+            />
+            <FormControlLabel
+              className='new-player__input'
+              control={
+                <Switch checked={this.state.broughtNotebook} value='broughtNotebook' onChange={::this.handleChange('broughtNotebook')}/>
+              }
+              label='Принёс ноутбук'
+            />
 
-        <Button disabled={disabled} className='new-player__input' onClick={::this.handleClick} variant='contained' color='primary'>
-          Зарегистрировать и показать ID
-        </Button>
-      </form>
+            <Button disabled={disabled} className='new-player__input' onClick={::this.handleClick} variant='contained' color='primary'>
+              Зарегистрировать и показать ID
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
     )
   }
 }
--- src/containers/Registration/components/RegisteredPlayer/index.js
+++ src/containers/Registration/components/RegisteredPlayer/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Button, FormControlLabel, Switch, TextField} from '@material-ui/core';
+import {Button, Card, CardContent, FormControlLabel, Switch, TextField} from '@material-ui/core';
 
 import './index.scss'
 
@@ -24,25 +24,29 @@ export default class RegisteredPlayer extends Component {
     const disabled = Object.keys(this.state).some(fieldName => (fieldName !== 'broughtNotebook' && !this.state[fieldName]));
 
     return (
-      <form className='registered-player'>
-        <h3 className='registered-player__header'>Регистрировался на сайте / получить ID</h3>
-        <TextField
-          className='new-player__input' type='email' label='E-mail'
-          variant='outlined' margin='dense'
-          value={this.state.email}
-          onChange={this.handleChange('email')}
-        />
-        <FormControlLabel
-          className='new-player__input'
-          control={
-            <Switch checked={this.state.broughtNotebook} value='broughtNotebook' onChange={this.handleChange('broughtNotebook')}/>
-          }
-          label='Принёс ноутбук'
-        />
-        <Button disabled={disabled} className='registered-player__input' onClick={::this.handleClick}  variant='contained' color='primary'>
-          Показать ID
-        </Button>
-      </form>
+      <Card>
+        <CardContent>
+          <form className='registered-player'>
+            <h3 className='registered-player__header'>Регистрировался на сайте / получить ID</h3>
+            <TextField
+              className='new-player__input' type='email' label='E-mail'
+              variant='outlined' margin='dense'
+              value={this.state.email}
+              onChange={this.handleChange('email')}
+            />
+            <FormControlLabel
+              className='new-player__input'
+              control={
+                <Switch checked={this.state.broughtNotebook} value='broughtNotebook' onChange={this.handleChange('broughtNotebook')}/>
+              }
+              label='Принёс ноутбук'
+            />
+            <Button disabled={disabled} className='registered-player__input' onClick={::this.handleClick}  variant='contained' color='primary'>
+              Показать ID
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
     )
   }
 }
