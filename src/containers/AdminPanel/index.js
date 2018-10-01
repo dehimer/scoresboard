0eaa3cc79790e4f8cddbe18cf8306819243981a9
Denis Novikov <den-vse@yandex.ru>
2018-10-02 00:17:25 +0300

    feature: finish with view of players table

--- src/containers/AdminPanel/index.js
+++ src/containers/AdminPanel/index.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import {
-  Paper, Table, TableHead,  TableBody, TableFooter,
+  Table, TableHead,  TableBody, TableFooter,
   TablePagination, TableRow, TableCell
 } from '@material-ui/core'
+
+import { withStyles } from '@material-ui/core/styles';
+const CustomTableCell = withStyles(theme => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white
+  },
+  body: {
+    fontSize: 14
+  }
+}))(TableCell);
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto'
+  },
+  table: {
+    minWidth: 700
+  },
+  row: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.background.default
+    }
+  }
+});
 
 import './index.scss'
 
@@ -45,67 +72,69 @@ class AdminPanel extends Component {
   }
 
   render() {
-    const { players_count: rowsLength=0, players: rows=[] } = this.props;
+    const { players_count: rowsLength=0, players: rows=[], classes } = this.props;
     const { rowsPerPage, page } = this.state;
 
     return (
       <div className='admin-panel'>
-        <Paper>
-          {
-            rows.length > 0 ? (
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>ID</TableCell>
-                    <TableCell>Ник</TableCell>
-                    <TableCell>ФИО</TableCell>
-                    <TableCell>День рождения</TableCell>
-                    <TableCell>Город</TableCell>
-                    <TableCell>Email</TableCell>
-                    <TableCell>Телефон</TableCell>
-                    <TableCell>Модель ноутбука</TableCell>
-                    <TableCell>Принёс ноутбук</TableCell>
-                    <TableCell>Ссылка на соцсеть</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {
-                    rows.map(row => {
-                      return (
-                        <TableRow key={row.code}>
-                          <TableCell numeric>{row.code}</TableCell>
-                          <TableCell>{row.nickname}</TableCell>
-                          <TableCell>{row.fullname}</TableCell>
-                          <TableCell>{row.birthday}</TableCell>
-                          <TableCell>{row.email}</TableCell>
-                          <TableCell>{row.city}</TableCell>
-                          <TableCell>{row.phone}</TableCell>
-                          <TableCell>{row.notebook}</TableCell>
-                          <TableCell>{ row.broughtNotebook ? 'Да' : 'Нет'}</TableCell>
-                          <TableCell>{row.link}</TableCell>
-                        </TableRow>
-                      );
-                    })
-                  }
-                </TableBody>
-                <TableFooter>
-                  <TableRow>
-                    <TablePagination
-                      colSpan={3}
-                      count={rowsLength}
-                      rowsPerPage={rowsPerPage}
-                      page={page}
-                      onChangePage={::this.handleChangePage}
-                      onChangeRowsPerPage={::this.handleChangeRowsPerPage}
-                    />
-                  </TableRow>
-                </TableFooter>
-              </Table>
-            ) : (
-              'Участников пока нет'
-            )
-          }
-        </Paper>
+        {
+          rows.length > 0 ? (
+            <Table className={classes.table}>
+              <TableHead>
+                <TableRow>
+                  <CustomTableCell>ID</CustomTableCell>
+                  <CustomTableCell>Ник</CustomTableCell>
+                  <CustomTableCell>ФИО</CustomTableCell>
+                  <CustomTableCell>День рождения</CustomTableCell>
+                  <CustomTableCell>Город</CustomTableCell>
+                  <CustomTableCell>Email</CustomTableCell>
+                  <CustomTableCell>Телефон</CustomTableCell>
+                  <CustomTableCell>Модель ноутбука</CustomTableCell>
+                  <CustomTableCell>Принёс ноутбук</CustomTableCell>
+                  <CustomTableCell>Ссылка на соцсеть</CustomTableCell>
+                  <CustomTableCell>Очки</CustomTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {
+                  rows.map(row => {
+                    return (
+                      <TableRow  className={classes.row} key={row.code}>
+                        <TableCell numeric>{row.code}</TableCell>
+                        <TableCell>{row.nickname}</TableCell>
+                        <TableCell>{row.fullname}</TableCell>
+                        <TableCell>{row.birthday.split('-').reverse().join('.')}</TableCell>
+                        <TableCell>{row.email}</TableCell>
+                        <TableCell>{row.city}</TableCell>
+                        <TableCell>{row.phone}</TableCell>
+                        <TableCell>{row.notebook}</TableCell>
+                        <TableCell>{ row.broughtNotebook ? 'Да' : 'Нет'}</TableCell>
+                        <TableCell>{row.link}</TableCell>
+                        <TableCell>{row.scores}</TableCell>
+                      </TableRow>
+                    );
+                  })
+                }
+              </TableBody>
+              <TableFooter>
+                <TableRow>
+                  <TablePagination
+                    component='div'
+                    className='admin-panel__pagination'
+                    colSpan={3}
+                    count={rowsLength}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onChangePage={::this.handleChangePage}
+                    onChangeRowsPerPage={::this.handleChangeRowsPerPage}
+                  />
+                </TableRow>
+              </TableFooter>
+            </Table>
+          ) : (
+            <div className='admin-panel__no-players'>Участников пока нет</div>
+          )
+        }
       </div>
     );
   }
@@ -134,4 +163,4 @@ const mapDispatchToProps = (dispatch) => {
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(AdminPanel);
+)(withStyles(styles)(AdminPanel));
