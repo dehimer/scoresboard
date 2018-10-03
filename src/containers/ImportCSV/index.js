e9176a0e19abb5ffa43013c53a1853e0e603e2aa
Denis Novikov <den-vse@yandex.ru>
2018-10-04 00:40:11 +0300

    feature: basic view of import csv page

--- src/containers/ImportCSV/index.js
+++ src/containers/ImportCSV/index.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import MenuBar from '../../components/MenuBar'
+import {Button, Card, CardContent, TextField} from '@material-ui/core';
+
+import './index.scss'
 
 export default () => (
   <div>
     <MenuBar/>
-    <div>ImportCSV</div>
+    <div className='import-csv-wrapper'>
+      <Card>
+        <CardContent>
+          <form className='import-csv'>
+            <h3 className='import-csv__header'>Импорт CSV</h3>
+
+            <TextField
+              className='import-csv__input' type='file'
+              variant='outlined' margin='dense'
+            />
+
+            <div className='import-csv__spacer'/>
+
+            <Button className='import-csv__input' variant='contained' color='primary'>
+              Импорт
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
   </div>
 )
