ae43bfa4be68c80d3c01af92e506ec750d9c7be8
Denis Novikov <den-vse@yandex.ru>
2018-11-26 04:50:34 +0300

    refactor: disable unneeded components

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Registration from '../Registration'
+/*
 import SetScores from '../SetScores'
-import SetTournamentNumber from '../SetTournamentNumber'
-import SetScoresLimit from '../SetScoresLimit'
 import AdminPanel from '../AdminPanel'
 import Top10Table from '../../components/Top10Table'
 import FullTable from '../../components/FullTable'
-import ImportCSV from '../ImportCSV'
+*/
 
 export default class App extends Component {
   render() {
@@ -15,13 +14,10 @@ export default class App extends Component {
       <div className='app'>
         <Switch>
           <Route exact path='/registration' component={ Registration } />
-          <Route exact path='/game' component={ SetScores } />
-          <Route exact path='/settournament' component={ SetTournamentNumber } />
-          <Route exact path='/setscoreslimit' component={ SetScoresLimit } />
-          <Route exact path='/adminpanel' component={ AdminPanel } />
-          <Route exact path='/table' component={ Top10Table } />
-          <Route exact path='/table-full' component={ FullTable } />
-          <Route exact path='/import-csv' component={ ImportCSV } />
+          {/*<Route exact path='/activity' component={ SetScores } />*/}
+          {/*<Route exact path='/adminpanel' component={ AdminPanel } />*/}
+          {/*<Route exact path='/table-5' component={ Top10Table } />*/}
+          {/*<Route exact path='/table-10' component={ FullTable } />*/}
         </Switch>
       </div>
     )
